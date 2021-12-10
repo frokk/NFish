74f3c31948aa43e332c64761eaf385984ba729b3
ADITYA <adityamishra081@protonmail.com>
2021-12-10 12:44:11 +0000

    Update ngrokHandler.js
    Without auth token ngrok worked, but today it didn't so i had to add the Auth Token.

--- src/ngrokHandler.js
+++ src/ngrokHandler.js
@@ -1,18 +1,26 @@
 async function connectToWorld(port) {
 	const ngrok = require('ngrok');
-	const url = await ngrok.connect({
-		addr: port || 80,
-		onStatusChange: (status) => {
-			if (status == "closed") {
-				console.log("Connection Lost, Trying To Reconnect...")
-			} else {
-				console.log("NGROK Started, Getting Public URL...");
-			}
-		},
-		onTerminated: () => console.log("NGROK Session Ended...")
-	});
+	const { authToken } = require('./ngrokAuthToken');
+	var url;
 
-	console.log("Public URL: " + url);
+	try {
+		url = await ngrok.connect({
+			addr: port || 80,
+			authtoken: authToken,
+			onStatusChange: (status) => {
+				if (status == "closed") {
+					console.log("Connection Lost, Trying To Reconnect...")
+				} else {
+					console.log("NGROK Started, Starting Tunneling...");
+				}
+			}
+			// onTerminated: () => console.log("NGROK Session Ended...")
+		});
+
+		console.log("Public URL: " + url);
+	} catch (err) {
+		console.log("NGROK ERROR: " + err.body.msg);
+	}
 }
 
 module.exports = connectToWorld
